c902a6295fbe33e376d6a1991a1faa8319284b3f
John Hagens <ginuwine104@gmail.com>
2023-02-02 17:18:28 -0500

    added .gitignore

--- server.js
+++ server.js
@@ -22,24 +22,6 @@ const db = mysql.createConnection(
   console.log(`Connected to the employees_db database.`)
 );
 
-// Create a employee db
-app.post('/api/new-movie', ({ body }, res) => {
-  const sql = `INSERT INTO movies (movie_name)
-    VALUES (?)`;
-  const params = [body.movie_name];
-  
-  db.query(sql, params, (err, result) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
-    }
-    res.json({
-      message: 'success',
-      data: body
-    });
-  });
-});
-
 // Read all movies
 app.get('/api/movies', (req, res) => {
   const sql = `SELECT id, movie_name AS title FROM movies`;
