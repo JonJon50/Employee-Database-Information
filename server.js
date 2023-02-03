1691b93c5d3a23757d8406eed51fec1b0ffb77d8
John Hagens <ginuwine104@gmail.com>
2023-02-02 23:07:03 -0500

    add question

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+const inquirer = require("inquirer")
 const express = require('express');
 // Import and require mysql2
 const mysql = require('mysql2');
+const { response } = require("express");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -22,86 +24,29 @@ const db = mysql.createConnection(
   console.log(`Connected to the employees_db database.`)
 );
 
-// Read all movies
-app.get('/api/movies', (req, res) => {
-  const sql = `SELECT id, movie_name AS title FROM movies`;
+// TODO: Create an array of questions for user input
+prompt([
+   {
+    type: "list",
+    message: "What would you like to do?",
+    name: "title",
+    choices: [{name: "View all employees?", value: "View employees?"},
+    {name: "Add employee?", value: "Add employee?"}, {name: "Update employee role?", value: "Update employee role?"}, 
+    {name: "View all roles?", value: "View all roles?"}, {name: "Add role?", value: "Add role?"}, {name: "View all department?", value: "View all department?"}, 
+    {name: "Add department?", value: "Add department?"},]
+   },
   
-  db.query(sql, (err, rows) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-       return;
-    }
-    res.json({
-      message: 'success',
-      data: rows
-    });
-  });
-});
-
-// Delete a movie
-app.delete('/api/movie/:id', (req, res) => {
-  const sql = `DELETE FROM movies WHERE id = ?`;
-  const params = [req.params.id];
-  
-  db.query(sql, params, (err, result) => {
-    if (err) {
-      res.statusMessage(400).json({ error: res.message });
-    } else if (!result.affectedRows) {
-      res.json({
-      message: 'Movie not found'
-      });
-    } else {
-      res.json({
-        message: 'deleted',
-        changes: result.affectedRows,
-        id: req.params.id
-      });
-    }
-  });
-});
-
-// Read list of all reviews and associated movie name using LEFT JOIN
-app.get('/api/movie-reviews', (req, res) => {
-  const sql = `SELECT movies.movie_name AS movie, reviews.review FROM reviews LEFT JOIN movies ON reviews.movie_id = movies.id ORDER BY movies.movie_name;`;
-  db.query(sql, (err, rows) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-    res.json({
-      message: 'success',
-      data: rows
-    });
-  });
-});
-
-// BONUS: Update review name
-app.put('/api/review/:id', (req, res) => {
-  const sql = `UPDATE reviews SET review = ? WHERE id = ?`;
-  const params = [req.body.review, req.params.id];
-
-  db.query(sql, params, (err, result) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-    } else if (!result.affectedRows) {
-      res.json({
-        message: 'Movie not found'
-      });
-    } else {
-      res.json({
-        message: 'success',
-        data: req.body,
-        changes: result.affectedRows
-      });
-    }
-  });
-});
-
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-  res.status(404).end();
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+   
+]).then(response => {let choice = response.choice}) 
+// TODO: Create a function to write writeME file
+  function init() {
+    inquirer.prompt(questions)
+    .then(function (ResponseFromInquirer) {
+    console.log(ResponseFromInquirer)
+    fs.writeFileSync("README.md", generateMarkdown(ResponseFromInquirer));
+    (err) =>
+    err ? console.log(err) : console.log("Successful");
+ });
+};
+// Function call to initialize app
+init();
